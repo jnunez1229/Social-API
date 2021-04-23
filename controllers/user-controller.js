62bde4684b98b69b01a67f1ec2d210c8a47f4875
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-04-22 22:02:30 -0500

    added routes for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -59,6 +59,6 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     }    
-
-
 }
+
+module.exports = userController;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://social-API',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-api',{
     useFindAndModify: false,
     useNewURLParser: true,
     useUnifiedTopology: true
